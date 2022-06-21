c267fa23b3f585a5ab1b4c31772960eea07c89a9
Kasper Skårhøj <kasper@skaarhoj.com>
2022-06-21 22:23:08 +0200

    Added Go Raw Panel API and example. Work in progress.

--- topology/topology.go
+++ topology/topology.go
@@ -1,10 +1,16 @@
 package topology
 
-// TODO: Import these definitions from somewhere else... (so it is shared)
+import (
+	"fmt"
+	"sync"
+)
+
 type Topology struct {
 	Title     string `json:"title,omitempty"` // Controller Title
 	HWc       []TopologyHWcomponent
 	TypeIndex map[uint32]TopologyHWcTypeDef `json:"typeIndex"`
+
+	sync.RWMutex `json:"title,omitempty"`
 }
 type TopologyHWcomponent struct {
 	Id           uint32              `json:"id"`   // The HWCid - follows the index (+1) of the $HWc
@@ -14,6 +20,176 @@ type TopologyHWcomponent struct {
 	Type         uint32              `json:"type"` // Type number, must be a key in $subElements (generateTopologies.phpsh) and thereby a key in the TypeIndex map. Type 0 (zero) means disabled.
 	TypeOverride *TopologyHWcTypeDef `json:"typeOverride,omitempty"`
 	UIparent     uint32              `json:"UIparent,omitempty"` // UI parent HWc, for simulator to know which elements should move along with a given parent when moved.
+}
+
+// See DC_SKAARHOJ_RawPanel.odt for descriptions:
+type TopologyHWcTypeDef struct {
+	W      int                         `json:"w,omitempty"`      // Width of component
+	H      int                         `json:"h,omitempty"`      // Height of component. If defined, the component will be a rectangle, otherwise a circle with diameter W.
+	Out    string                      `json:"out,omitempty"`    // Output type
+	In     string                      `json:"in,omitempty"`     // Input type
+	Desc   string                      `json:"desc,omitempty"`   // Description
+	Ext    string                      `json:"ext,omitempty"`    // Extended return value mode
+	Subidx int                         `json:"subidx,omitempty"` // A reference to the index of an element in the "sub" element which has a "special" meaning. For analog (av, ah, ar) and intensity (iv, ih, ir) elements, this would be an element suggested for being used as a handle for a fader or joystick.
+	Rotate float32                     `json:"rotate,omitempty"`
+	Disp   *TopologyHWcTypeDef_Display `json:"disp,omitempty"` // Display description
+	Sub    []TopologyHWcTypeDefSubEl   `json:"sub,omitempty"`
+
+	sync.RWMutex `json:"title,omitempty"`
+}
+
+type TopologyHWcTypeDefSubEl struct {
+	ObjType string `json:"_,omitempty"`
+	X       int    `json:"_x,omitempty"`
+	Y       int    `json:"_y,omitempty"`
+	W       int    `json:"_w,omitempty"`
+	H       int    `json:"_h,omitempty"`
+	R       int    `json:"r,omitempty"`
+	Rx      int    `json:"rx,omitempty"`
+	Ry      int    `json:"ry,omitempty"`
+	Style   string `json:"style,omitempty"`
+	Idx     int    `json:"_idx,omitempty"`
+}
+type TopologyHWcTypeDef_Display struct {
+	W      int    `json:"w,omitempty"`         // Pixel width of display
+	H      int    `json:"h,omitempty"`         // Pixel height of display
+	Subidx int    `json:"subidx,omitempty"`    // Index of the sub element which placeholds for the display area. -1 if no sub element is used for that
+	Type   string `json:"type,omitempty"`      // Additional features of display. "color" for example.
+	Shrink int    `json:"shrink,omitempty"`    // W+H Shrink. W=bit0, H=bit1. W-shrink cuts a pixel off in the right side of tile. H-shrink cuts a pixel off in the bottom of tile.
+	Border int    `json:"txtborder,omitempty"` // Txt Border (shall match that used by ibeam-hardware and UniSketch for rendering)
+}
+
+// Returns the hardware component ids of the topology (should range successively from 1 to the number of components on the panel) and there should really be no duplicates inside.
+func (topology *Topology) GetHWCs() []uint32 {
+	topology.Lock()
+	defer topology.Unlock()
+
+	retval := []uint32{}
+	for _, hwcComp := range topology.HWc {
+		retval = append(retval, hwcComp.Id)
+	}
+	return retval
+}
+
+// Returns X,Y coordinates of the components (1/10 of a mm)
+func (topology *Topology) GetHWCxy(hwc uint32) (int, int) {
+	topology.Lock()
+	defer topology.Unlock()
+
+	for _, HWcDef := range topology.HWc {
+		if HWcDef.Id == hwc {
+			return HWcDef.X, HWcDef.Y
+		}
+	}
+	return -1, -1
+}
+
+// Returns the Txt label of the hardware component
+func (topology *Topology) GetHWCtext(hwc uint32) string {
+	topology.Lock()
+	defer topology.Unlock()
+
+	for _, HWcDef := range topology.HWc {
+		if HWcDef.Id == hwc {
+			return HWcDef.Txt
+		}
+	}
+	return ""
+}
+
+// Returns the HWC Type Definition, overridden with any customization
+func (topology *Topology) GetHWCtype(hwc uint32) (*TopologyHWcTypeDef, error) {
+	topology.Lock()
+	defer topology.Unlock()
+
+	for _, HWcDef := range topology.HWc {
+		if HWcDef.Id == hwc {
+			typeDef := topology.getTypeDefWithOverride(&HWcDef)
+			return &typeDef, nil
+		}
+	}
+	return nil, fmt.Errorf("No HWC found")
+}
+
+func (typeDef *TopologyHWcTypeDef) IsButton() bool {
+	typeDef.Lock()
+	defer typeDef.Unlock()
+	return typeDef.isButton()
+}
+
+func (typeDef *TopologyHWcTypeDef) isButton() bool {
+	return typeDef.In == "b" || typeDef.In == "b4" || typeDef.In == "b2h" || typeDef.In == "b2v" || typeDef.In == "pb"
+}
+
+func (typeDef *TopologyHWcTypeDef) IsBinary() bool {
+	typeDef.Lock()
+	defer typeDef.Unlock()
+	return typeDef.isButton() || typeDef.In == "pb"
+}
+
+func (typeDef *TopologyHWcTypeDef) IsPulsed() bool {
+	typeDef.Lock()
+	defer typeDef.Unlock()
+	return typeDef.In == "pb" || typeDef.In == "p"
+}
+
+func (typeDef *TopologyHWcTypeDef) IsAbsolute() bool {
+	typeDef.Lock()
+	defer typeDef.Unlock()
+	return typeDef.In == "av" || typeDef.In == "ah" || typeDef.In == "ar"
+}
+
+func (typeDef *TopologyHWcTypeDef) IsIntensity() bool {
+	typeDef.Lock()
+	defer typeDef.Unlock()
+	return typeDef.In == "iv" || typeDef.In == "ih" || typeDef.In == "ir"
+}
+
+/*
+func (topology *Topology) DisplayInfo(hwc uint32) bool {
+}
+func (topology *Topology) HasLED(hwc uint32) bool {
+}
+func (topology *Topology) HasSteps(hwc uint32) bool {
+}
+func (topology *Topology) IsMotorized(hwc uint32) bool {
+}
+*/
+
+func (topology *Topology) Verify() {
+
+	uniqueIDs := make(map[uint32]bool)
+	typeCount := make(map[uint32]int)
+	for _, HWc := range topology.HWc {
+		// Check uniqueness of ids:
+		if _, ok := uniqueIDs[HWc.Id]; !ok {
+			uniqueIDs[HWc.Id] = true
+		} else {
+			fmt.Printf("ERROR: ID %d listed multiple times\n", HWc.Id)
+		}
+
+		// Check availability of typeIndex:
+		if HWc.Type != 0 { // If not disabled.
+			if _, ok := topology.TypeIndex[HWc.Type]; !ok {
+				fmt.Printf("ERROR: Type %d not found in type index\n", HWc.Type)
+			} else {
+				typeCount[HWc.Type]++
+				/*
+					if HWc.TypeOverride.Subidx >= 0 {
+						if len(topology.TypeIndex[HWc.Type].Sub) <= HWc.TypeOverride.Subidx {
+							fmt.Printf("ERROR: Subidx %d not found in type index\n", HWc.TypeOverride.Subidx)
+						}
+					}
+				*/
+			}
+		}
+	}
+
+	for key, _ := range topology.TypeIndex {
+		if _, ok := typeCount[key]; !ok {
+			fmt.Printf("Warning: Type %d not used in HWcID index\n", key)
+		}
+	}
 }
 
 func (topology *Topology) getTypeDefWithOverride(HWcDef *TopologyHWcomponent) TopologyHWcTypeDef {
@@ -53,39 +229,4 @@ func (topology *Topology) getTypeDefWithOverride(HWcDef *TopologyHWcomponent) To
 	}
 
 	return typeDef
-}
-
-// See DC_SKAARHOJ_RawPanel.odt for descriptions:
-type TopologyHWcTypeDef struct {
-	W      int                         `json:"w,omitempty"`      // Width of component
-	H      int                         `json:"h,omitempty"`      // Height of component. If defined, the component will be a rectangle, otherwise a circle with diameter W.
-	Out    string                      `json:"out,omitempty"`    // Output type
-	In     string                      `json:"in,omitempty"`     // Input type
-	Desc   string                      `json:"desc,omitempty"`   // Description
-	Ext    string                      `json:"ext,omitempty"`    // Extended return value mode
-	Subidx int                         `json:"subidx,omitempty"` // A reference to the index of an element in the "sub" element which has a "special" meaning. For analog (av, ah, ar) and intensity (iv, ih, ir) elements, this would be an element suggested for being used as a handle for a fader or joystick.
-	Rotate float32                     `json:"rotate,omitempty"`
-	Disp   *TopologyHWcTypeDef_Display `json:"disp,omitempty"` // Display description
-	Sub    []TopologyHWcTypeDefSubEl   `json:"sub,omitempty"`
-}
-
-type TopologyHWcTypeDefSubEl struct {
-	ObjType string `json:"_,omitempty"`
-	X       int    `json:"_x,omitempty"`
-	Y       int    `json:"_y,omitempty"`
-	W       int    `json:"_w,omitempty"`
-	H       int    `json:"_h,omitempty"`
-	R       int    `json:"r,omitempty"`
-	Rx      int    `json:"rx,omitempty"`
-	Ry      int    `json:"ry,omitempty"`
-	Style   string `json:"style,omitempty"`
-	Idx     int    `json:"_idx,omitempty"`
-}
-type TopologyHWcTypeDef_Display struct {
-	W      int    `json:"w,omitempty"`         // Pixel width of display
-	H      int    `json:"h,omitempty"`         // Pixel height of display
-	Subidx int    `json:"subidx,omitempty"`    // Index of the sub element which placeholds for the display area. -1 if no sub element is used for that
-	Type   string `json:"type,omitempty"`      // Additional features of display. "color" for example.
-	Shrink int    `json:"shrink,omitempty"`    // W+H Shrink. W=bit0, H=bit1. W-shrink cuts a pixel off in the right side of tile. H-shrink cuts a pixel off in the bottom of tile.
-	Border int    `json:"txtborder,omitempty"` // Txt Border (shall match that used by ibeam-hardware and UniSketch for rendering)
 }
